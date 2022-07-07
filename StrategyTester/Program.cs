6e88691f7bdf6233b499d5b3c7ff27fab06fe9fd
YellowFive <yellowfive555@yahoo.com>
2022-07-07 23:00:14 +0300

    StrategyTester init

---
+++ StrategyTester/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+#region Usings
+
+using System;
+
+#endregion
+
+namespace StrategyTester;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.ReadKey();
+    }
+}
