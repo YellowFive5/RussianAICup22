3326aab4c5ca15a8ddcf40176e6d7808a83d936a
YellowFive <yellowfive555@yahoo.com>
2022-07-07 22:23:54 +0300

    Cleanups

--- client-csharp/MyStrategy.cs
+++ client-csharp/MyStrategy.cs
@@ -1,13 +1,29 @@
+#region Usings
+
+using System.Collections.Generic;
+using AiCup22.Model;
+
+#endregion
+
 namespace AiCup22
 {
     public class MyStrategy
     {
-        public MyStrategy(AiCup22.Model.Constants constants) {}
-        public AiCup22.Model.Order GetOrder(AiCup22.Model.Game game, DebugInterface debugInterface)
+        public MyStrategy(Constants constants)
         {
-            return new AiCup22.Model.Order(new System.Collections.Generic.Dictionary<int, AiCup22.Model.UnitOrder>());
         }
-        public void DebugUpdate(DebugInterface debugInterface) {}
-        public void Finish() {}
+
+        public Order GetOrder(Game game, DebugInterface debugInterface)
+        {
+            return new Order(new Dictionary<int, UnitOrder>());
+        }
+
+        public void DebugUpdate(DebugInterface debugInterface)
+        {
+        }
+
+        public void Finish()
+        {
+        }
     }
 }
--- client-csharp/Runner.cs
+++ client-csharp/Runner.cs
@@ -1,30 +1,35 @@
+#region Usings
+
 using System;
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Net.Sockets;
 using System.Text;
+using AiCup22.Codegame;
+
+#endregion
 
 namespace AiCup22
 {
     public class Runner
     {
-        private BinaryReader reader;
-        private BinaryWriter writer;
+        private readonly BinaryReader reader;
+        private readonly BinaryWriter writer;
+
         public Runner(string host, int port, string token)
         {
             var client = new TcpClient(host, port) { NoDelay = true };
             var stream = new BufferedStream(client.GetStream());
             reader = new BinaryReader(stream);
             writer = new BinaryWriter(stream);
-            var tokenData = System.Text.Encoding.UTF8.GetBytes(token);
+            var tokenData = Encoding.UTF8.GetBytes(token);
             writer.Write(tokenData.Length);
             writer.Write(tokenData);
-            writer.Write((int) 1);
-            writer.Write((int) 0);
-            writer.Write((int) 1);
+            writer.Write(1);
+            writer.Write(0);
+            writer.Write(1);
             writer.Flush();
         }
+
         public void Run()
         {
             MyStrategy myStrategy = null;
@@ -32,22 +37,25 @@ namespace AiCup22
             var running = true;
             while (running)
             {
-                switch (AiCup22.Codegame.ServerMessage.ReadFrom(reader))
+                switch (ServerMessage.ReadFrom(reader))
                 {
-                    case AiCup22.Codegame.ServerMessage.UpdateConstants message:
+                    case ServerMessage.UpdateConstants message:
                         myStrategy = new MyStrategy(message.Constants);
                         break;
-                    case AiCup22.Codegame.ServerMessage.GetOrder message:
-                        new AiCup22.Codegame.ClientMessage.OrderMessage(myStrategy.GetOrder(message.PlayerView, message.DebugAvailable ? debugInterface : null)).WriteTo(writer);
+                    case ServerMessage.GetOrder message:
+                        new ClientMessage.OrderMessage(myStrategy.GetOrder(message.PlayerView,
+                                                                           message.DebugAvailable
+                                                                               ? debugInterface
+                                                                               : null)).WriteTo(writer);
                         writer.Flush();
                         break;
-                    case AiCup22.Codegame.ServerMessage.Finish message:
+                    case ServerMessage.Finish message:
                         running = false;
                         myStrategy.Finish();
                         break;
-                    case AiCup22.Codegame.ServerMessage.DebugUpdate message:
+                    case ServerMessage.DebugUpdate message:
                         myStrategy.DebugUpdate(debugInterface);
-                        new AiCup22.Codegame.ClientMessage.DebugUpdateDone().WriteTo(writer);
+                        new ClientMessage.DebugUpdateDone().WriteTo(writer);
                         writer.Flush();
                         break;
                     default:
@@ -55,11 +63,18 @@ namespace AiCup22
                 }
             }
         }
+
         public static void Main(string[] args)
         {
-            string host = args.Length < 1 ? "127.0.0.1" : args[0];
-            int port = args.Length < 2 ? 31001 : int.Parse(args[1]);
-            string token = args.Length < 3 ? "0000000000000000" : args[2];
+            string host = args.Length < 1
+                              ? "127.0.0.1"
+                              : args[0];
+            int port = args.Length < 2
+                           ? 31001
+                           : int.Parse(args[1]);
+            string token = args.Length < 3
+                               ? "0000000000000000"
+                               : args[2];
             new Runner(host, port, token).Run();
         }
     }
